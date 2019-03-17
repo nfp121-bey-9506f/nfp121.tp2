2015bd4ad604357948804f6d7ed9f622014d94e1
Hassane Jalal <jalal.hassane@isae.edu.lb>
2019-03-17 17:32:33 +0200

    Question 2 finie

--- tp2/question2/FahrenheitCelsius.java
+++ tp2/question2/FahrenheitCelsius.java
@@ -2,21 +2,25 @@ package question2;
 
 
 /**
- * Décrivez votre classe FahrenheitCelsius ici.
- * 
- * @author (votre nom) 
- * @version (un numéro de version ou une date)
+ * classe FahrenheitCelsius sert à convertir une température en echelle Fahrenheit à une température en echelle Celsius.
+ * les températures à convertir sont passées des la ligne de commande en gérant les exceptions NumberFormatException
+ * @author Jalal Hassane
+ * @version 17/3/2019
  */
 public class FahrenheitCelsius{
 
-     /** le point d'entrée de cette application, 
-      * dont le commentaire est à compléter
+     /** 
+      * 
       *
-      *  @param args ...
+      *  @param args contient les paramètres saisies à partir de la ligne de commande
       */
      public static void main(String[] args){
        try{
-       
+       for(int i =0;i<args.length;i++){
+            int fahrenheit = Integer.parseInt(args[i]);
+            float celsius = fahrenheitEnCelsius(fahrenheit);
+            System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C");
+        }
       
        }catch(NumberFormatException nfe){
            System.out.println("error : " + nfe.getMessage());  // en cas d'erreur 
@@ -30,9 +34,9 @@ public class FahrenheitCelsius{
       *   @return  la conversion en degré Celsius
       */
      public static float fahrenheitEnCelsius( int f){
-       // ...
-       return 0.F;	// à compléter	en remplaçant la valeur retournée par la fonction de conversion
-       // ...
+       double celsius =  5.0/9 * (f - 32);            //conversion de fahrenheit en celsius
+        celsius = ((int)(celsius*10))/10.0;            //nombre avec un seul chiffre apres la virgule
+        return (float)celsius;
      }
 
 }
--- tp2/question2/FahrenheitCelsiusTest.java
+++ tp2/question2/FahrenheitCelsiusTest.java
@@ -4,8 +4,8 @@ package question2;
 /**
  * Classe-test FahrenheitCelsiusTest.
  *
- * @author  (votre nom)
- * @version (un numéro de version ou une date)
+ * @author  Jalal Hassane
+ * @version 17/3/2019
  *
  * Les classes-test sont documentées ici :
  * http://junit.sourceforge.net/javadoc/junit/framework/TestCase.html
@@ -32,7 +32,8 @@ public class FahrenheitCelsiusTest extends junit.framework.TestCase
     // à l'aide du menu contextuel "Présentoir --> Engagements".
     // Notez cependant que ce dernier ne peut saisir les objets primitifs
     // du présentoir (les objets sans constructeur, comme int, float, etc.).
-
+    private int fahrenheitTest1,fahrenheitTest2,fahrenheitTest3,fahrenheitTest4,fahrenheitTest5;
+    private int fahrenheitTest6,fahrenheitTest7,fahrenheitTest8,fahrenheitTest9,fahrenheitTest10;
     /**
      * Constructeur de la classe-test FahrenheitCelsiusTest
      */
@@ -48,7 +49,16 @@ public class FahrenheitCelsiusTest extends junit.framework.TestCase
     protected void setUp() // throws java.lang.Exception
     {
         // Initialisez ici vos engagements
-
+        fahrenheitTest1 = 5;
+        fahrenheitTest2 = 202;
+        fahrenheitTest3 = -100;
+        fahrenheitTest4 = -1;
+        fahrenheitTest5 = 10000;
+        fahrenheitTest6 = 33;
+        fahrenheitTest7 = 92;
+        fahrenheitTest8 = 37;
+        fahrenheitTest9 = 0;
+        fahrenheitTest10 = 2;
     }
 
     /**
@@ -59,6 +69,16 @@ public class FahrenheitCelsiusTest extends junit.framework.TestCase
     protected void tearDown() // throws java.lang.Exception
     {
         //Libérez ici les ressources engagées par setUp()
+        fahrenheitTest1=0;
+        fahrenheitTest2=0;
+        fahrenheitTest3=0;
+        fahrenheitTest4=0;
+        fahrenheitTest5=0;
+        fahrenheitTest6=0;
+        fahrenheitTest7=0;
+        fahrenheitTest8=0;
+        fahrenheitTest9=0;
+        fahrenheitTest10=0;
     }
 
     /**
@@ -68,5 +88,20 @@ public class FahrenheitCelsiusTest extends junit.framework.TestCase
      * Par convention, leurs noms devraient débuter par "test".
      * Vous pouvez ébaucher le corps grâce au menu contextuel "Enregistrer une méthode de test".
      */
+public void test_fahrenheitEnCelcius1(){
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest1) == -15.0);
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest2) == 94.4);
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest3) == -73.3);
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest4) == -18.3);
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest5) == 5537.7);
 
+    }
+    public void test_fahrenheitEnCelcius2(){
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest5) == 0.5);
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest6) == 33.3);
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest7) == 2.7);
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest8) == -17.7);
+        assertTrue(question1.FahrenheitCelsius.fahrenheitEnCelsius(fahrenheitTest9) == -16.6);
+
+    }
 }
